3b24aa9d22e5510cea2d5c74383ca433cd479147
Rameen Aryanpur <raryanpur@aqatechnologies.com>
2025-03-10 23:17:58 -0400

    dev: add configuration and artifacts to application

--- recipes/system-ota-manager/aqago-infra.sh
+++ recipes/system-ota-manager/aqago-infra.sh
@@ -3,7 +3,21 @@
 # Configure the cli with aqago-cli configure prior to running this script and
 # replace the device id with an actual device id
 
-DEVICE_ID="replace with device id"
+DEVICE_ID=$1
 
-aqago-cli applications create system-ota-manager-rugix --artifact rugix-system-bundle:latest
-aqago-cli devices add-application $DEVICE_ID system-ota-manager-rugix
+./aqago-cli applications create system-ota-manager-rugix\
+    --artifact rugix-os-bundle:latest~efi-arm64\
+    --artifact rugix-os-bundle:latest~efi-amd64\
+    --artifact rugix-os-bundle:latest~pi4\
+    --artifact rugix-os-bundle:latest~pi5\
+    --configuration ./configuration/default.json\
+    --configuration-schema ./configuration/schema.json
+
+if [ $? -ne 0 ]; then
+    echo "Failed to create application"
+    exit 1
+fi
+if [ -z "$DEVICE_ID" ]; then
+    exit 0
+fi
+./aqago-cli devices add-application $DEVICE_ID system-ota-manager-rugix
